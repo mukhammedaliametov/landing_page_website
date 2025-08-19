1c132dd9db2b53bdf8c431eb070160e77a107b8e
mukhammedaliametov <ametovmukhammedali07@gmail.com>
2025-08-20 04:53:43 +0500

    create header and home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import HeroBg from "../assets/hero_bg.png";
-import MobileBg from '../assets/mobile_bg.jpg';
-import { IoMdMenu } from "react-icons/io";
+import MobileBg from "../assets/mobile_bg.jpg";
+import { IoMdMenu, IoMdClose } from "react-icons/io";
 
 const Hero = () => {
+    const [nav, setNav] = useState(0);
+  const navItems = [
+    { name: "Home", link: "#home" },
+    { name: "About Me", link: "#aboutme" },
+    { name: "Skills", link: "#skills" },
+    { name: "Portfolio", link: "#portfolio" },
+    { name: "Contacts", link: "#contacts" },
+  ];
+  const handleClick = () => setNav(!nav)
+  const closeNav = () => setNav(false);
   return (
     <section id="home" className="max-w-[1100px] mx-auto px-[16px] md:px-0">
       <div className="flex items-start justify-between">
@@ -24,12 +34,41 @@ const Hero = () => {
             </p>
           </div>
         </div>
-          <div className="block md:hidden text-[38px] mt-[45px]">
-            <IoMdMenu />
+        <div className="block md:hidden text-[38px] mt-[45px]" onClick={handleClick}>
+          <IoMdMenu />
+        </div>
+        <nav className={`block md:hidden absolute font-poppins w-full top-0 bg-white h-[100vh] pt-[50px] transition-all duration-300 ${nav ? 'translate-x-0' : '-translate-x-full'}`}>
+          <ul className="flex flex-col gap-[30px] ml-[10px]">
+            {navItems.map((item, index) => {
+              return (
+                <li key={index} onClick={closeNav}>
+                  <a href={item.link} className="text-[32px] font-semibold text-[#828282]">
+                    {item.name}
+                  </a>
+                </li>
+              );
+            })}
+            <div className="absolute right-0 text-[38px] mr-[30px] mt-[-5px] text-[#828282]" onClick={closeNav}>
+                <IoMdClose />
+            </div>
+            <div className="absolute bottom-[50px] right-0 mr-[30px] flex flex-col items-center font-semibold gap-[12px] uppercase">
+            <p className="text-[34px] cursor-pointer [writing-mode:vertical-rl]">
+              Eng
+            </p>
+            <span className="w-[28px] h-[3px] bg-black"></span>
+            <p className="text-[34px] cursor-pointer text-[#828282] [writing-mode:vertical-rl]">
+              Ru
+            </p>
           </div>
+          </ul>
+        </nav>
       </div>
       <div>
-        <img src={HeroBg} alt="hero_bg" className="hidden md:block w-full h-[440px]" />
+        <img
+          src={HeroBg}
+          alt="hero_bg"
+          className="hidden md:block w-full h-[440px]"
+        />
         <img src={MobileBg} alt="mobile_bg" className="block md:hidden" />
       </div>
     </section>
